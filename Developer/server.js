8ccc45e51dafece8e87046579cf5a96694fa3da2
Call Carl <callienicole0419@gmail.com>
2023-01-30 13:53:36 -0600

    added uuid and post request in progress

--- Developer/server.js
+++ Developer/server.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const path = require('path');
 
+//uuid for generating a unique note id
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); 
+
 const database = require('./db/db.json');
 
 const app = express();
@@ -23,10 +27,37 @@ app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+//reads db.json file and returns as json
 app.get('/api/notes', (req, res) =>
     res.json(database)
 ); 
 
+//receives a new note to save on request body, add to db.json, and return new note to client
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note.`);
+  
+    // Check if there is anything in the response body
+    if (req.body && req.body.title) {
+      response = {
+        status: 'success',
+        data: req.body,
+      };
+      res.json(`Note: ${response.data.title}, ${response.data.text} has been added!`);
+    } else {
+      res.json('Note must contain a title and text.');
+    }
+  
+    // Log the response body to the console
+    console.log(req.body);
+
+});
+
+//for loop for notes entered
+// for(let i = 0; i < database.length; i++){
+
+//}
+// uuid for uniquie id for notes
 //app.get('*', (req, res) => 
 //res.sendFile(path.join(__dirname, 'public/index.html'))
 //);
